bfcffe0b06f198379f0974be893b8bdf81d84ae3
Johan Paul Barahona Santos <72539686+panlar@users.noreply.github.com>
2022-05-25 08:57:18 -0600

    Update lazyloading.js

--- lazyloading.js
+++ lazyloading.js
@@ -1,30 +1,17 @@
-function lazyLoading (elements = 'img', mode = 'src') {
+const loadingImg = (mode) => (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      mode === 'src'
+        ? (entry.target.src = entry.target.dataset.src)
+        : (entry.target.style.backgroundImage = `url("${entry.target.dataset.src}")`);
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const lazyLoading = (elements = 'img', mode = 'src') => {
   const els = document.querySelectorAll(elements);
-  let Observer;
+  let observer = new IntersectionObserver(loadingImg(mode));
 
-  if (mode === 'src') {
-    Observer = new IntersectionObserver(loadingSRC);
-  } else if (mode === 'bg') {
-    Observer = new IntersectionObserver(loadingBG);
-  }
-
-  els.forEach(el => Observer.observe(el))
-}
-
-function loadingSRC (entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        entry.target.src = entry.target.dataset.src;
-        observer.unobserve(entry.target);
-    };
-  });
-}
-
-function loadingBG (entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        entry.target.style.backgroundImage = `url("${entry.target.dataset.src}")`
-        observer.unobserve(entry.target);
-    };
-  });
-}
+  els.forEach((el) => observer.observe(el));
+};
